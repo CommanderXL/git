ae497d23bf667a7fa8ad5833809c305f35ffa911
xiaolei <1017653702@qq.com>
2019-11-17 10:42:59 +0800

    [update]
    [update]1
    
    Revert "[update]"
    
    This reverts commit 96b1d6e7e43ca09deb018d6481cbffbbeade7da9.
    
    [update]

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ function a () {
     console.log(123)
     console.log(456)
     console.log(789)
-    console.log(11)
-    console.log(22)
-    console.log(33)
-    console.log(44)
-    console.log(55)
-    console.log(66)
 }

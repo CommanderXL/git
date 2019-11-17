64e0f69eda56dfdf7357a1339b1a1f4f811073d1
xiaolei <1017653702@qq.com>
2019-11-17 10:44:40 +0800

    Revert "[update]"
    This reverts commit 96b1d6e7e43ca09deb018d6481cbffbbeade7da9.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const argv = require('minimist')(process.argv.slice(2))
 function a () {
     console.log(123)
     console.log(456)
+    console.log(789)
+    console.log(11)
     console.log(22)
     console.log(33)
     console.log(66)
 }
-console.log(argv)

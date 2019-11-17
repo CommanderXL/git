96b1d6e7e43ca09deb018d6481cbffbbeade7da9
xiaolei <1017653702@qq.com>
2019-11-17 10:42:59 +0800

    [update]

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+const argv = require('minimist')(process.argv.slice(2))
 function a () {
     console.log(123)
     console.log(456)
-    console.log(789)
-    console.log(11)
     console.log(22)
     console.log(33)
     console.log(44)
     console.log(55)
     console.log(66)
 }
+console.log(argv)
